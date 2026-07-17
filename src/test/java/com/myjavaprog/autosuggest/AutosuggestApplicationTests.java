f2187f76a372638a8546a38711b63056fc19c249
Nandu Sai Bommalata <nandusaibommalata122008@gmail.com>
2026-07-17 12:51:10 +0530

    setup

---
+++ src/test/java/com/myjavaprog/autosuggest/AutosuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjavaprog.autosuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
