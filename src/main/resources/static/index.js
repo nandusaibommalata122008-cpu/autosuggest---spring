93ed6b9a61c1afde22d7c5a26dde07aa7f07f882
Nandu Sai Bommalata <nandusaibommalata122008@gmail.com>
2026-07-18 12:21:59 +0530

    commit all the changes

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,36 @@
- alert("hello from js");
+// alert("Hello guysss");
 
+console.log("hello from JS!")
+
+var users = [
+    {
+        "name" : "Nandu sai",
+        "gender" : "Male",
+        "img" : "25071A6743.jpeg"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.jpg"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
